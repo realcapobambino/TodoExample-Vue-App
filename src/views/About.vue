60152bc4d52c5dc4b6ff1178e420187e15d7294a
unknown <charliealienchamp@gmail.com>
2020-02-04 11:43:07 +0300

    second commit - complete app with vue router

---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+
+    <p>This is my first Vue App..... 
+
+      Sorry the delete buttons are anot functioning
+
+      I'm on it!!
+    </p>
+
+  </div>
+</template>
